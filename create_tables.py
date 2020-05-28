79de78b3c9011a7ff90411045eb3c72902a8870c
DanJoJo <lhongyu0509@gmail.com>
2020-05-27 18:28:06 -0700

    update docstrings

--- create_tables.py
+++ create_tables.py
@@ -4,6 +4,10 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drop all tables if previously created in the redshift for avoid conflicting errors
+    :param cur: current cursor for the database
+    :param conn: databse on the cloud
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
@@ -11,6 +15,10 @@ def drop_tables(cur, conn):
 
 
 def create_tables(cur, conn):
+    """Create tables in the redshift cluster for saving result of ETL processes
+    :param cur: current cursor for the database
+    :param conn: databse on the cloud
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
@@ -18,16 +26,21 @@ def create_tables(cur, conn):
 
 
 def main():
+    # Get data warehouse parameters on the redshift cluster
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Connect to the database
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
         *config['CLUSTER'].values()))
     cur = conn.cursor()
 
+    # Drop the tables in the cluster
     drop_tables(cur, conn)
+    # Create table in the cluster
     create_tables(cur, conn)
 
+    # Disconnect to the database
     conn.close()
 
 
--- etl.py
+++ etl.py
@@ -5,6 +5,11 @@ import time
 
 
 def load_staging_tables(cur, conn):
+    """Copying data from S3 bucket into the staging tables in the redshift cluster
+    and then use for dimention and fact tables
+    :param cur: current cursor for the database
+    :param conn: databse on the cloud
+    """
     for query, name in copy_table_queries:
         print('Start to copy {} table from S3...'.format(name))
         start = time.time()
@@ -14,6 +19,11 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """Insert data from staging table in the cluster into dimension and fact tables
+    in the appropriate types
+    :param cur: current cursor for the database
+    :param conn: databse on the cloud
+    """
     for query, name in insert_table_queries:
         print('Start to insert into {} table...'.format(name))
         start = time.time()
@@ -23,16 +33,21 @@ def insert_tables(cur, conn):
 
 
 def main():
+    # Get data warehouse parameter in the cluster
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Connect to the database in the cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
         *config['CLUSTER'].values()))
     cur = conn.cursor()
 
+    # Copy data into staging tables
     load_staging_tables(cur, conn)
+    # Insert data into dimension and fact tables
     insert_tables(cur, conn)
 
+    # Disconnect to the database in the cluster
     conn.close()
 
 
